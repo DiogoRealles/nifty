49d4f634d87f149beaffcda44eb101ae84a3470f
Diogo Realles <diogorealles@hotmail.com>
2024-05-08 16:54:08 -0300

    WIP

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+import fnSlideProducts from "./modules/slide-products.js";
+import accordion from "./modules/accordion.js";
+import aosjs from "./modules/aos.js";
+
+fnSlideProducts();
+accordion();
+aosjs();
